646dc010f65d042d7c3272338b14102d41b1b6a0
Alaggesan Palani <alagesann@gmail.com>
2016-08-03 11:03:46 +0530

    Dose calculator service to work with rules engine and request parameter is changed to request object

--- bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/controller/DoseCalculatorController.java
+++ bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/controller/DoseCalculatorController.java
@@ -1,6 +1,7 @@
 package org.bahmni.module.bahmnicore.web.v1_0.controller;
 
 import org.openmrs.api.APIException;
+import org.openmrs.module.rulesengine.domain.DosageRequest;
 import org.openmrs.module.rulesengine.domain.Dose;
 import org.openmrs.module.rulesengine.engine.RulesEngine;
 import org.openmrs.module.rulesengine.rule.BSABasedDoseRule;
@@ -9,10 +10,7 @@ import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.v1_0.controller.BaseRestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping(value = "/rest/" + RestConstants.VERSION_1 + "/bahmnicore/calculateDose")
@@ -23,21 +21,7 @@ public class DoseCalculatorController extends BaseRestController {
 
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
-    public Dose calculateDose(@RequestParam(value = "patientUuid") String patientUuid,
-                              @RequestParam(value = "drugName") String drugName,
-                              @RequestParam(value = "baseDose") Double baseDose,
-                              @RequestParam(value = "doseUnit") String doseUnit,
-                              @RequestParam(value = "orderSetName") String orderSetName) throws Exception {
-
-        return rulesEgine.calculateDose(patientUuid,drugName, baseDose, doseUnit, orderSetName);
-
-//        if("mg/kg".equals(doseUnit)){
-//            return WeightBasedDoseRule.calculateDose(patientUuid, baseDose);
-//        }
-//        if("mg/m2".equals(doseUnit)){
-//            return BSABasedDoseRule.calculateDose(patientUuid, baseDose);
-//        }
-//        String errMessage = "Dosing Rule not found for given doseUnits (" + doseUnit + ").";
-//        throw new APIException(errMessage);
+    public Dose calculateDose(@RequestBody DosageRequest request) throws Exception {
+        return rulesEgine.calculateDose(request);
     }
 }

2af276640a8dea91580170251126be0f57fa74de
Alaggesan Palani <alagesann@gmail.com>
2016-08-10 11:49:04 +0530

    Drug order rules final changes and liquibase migration scripts to add concepts and global property for the concept uuid

--- bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/controller/DoseCalculatorController.java
+++ bahmnicore-omod/src/main/java/org/bahmni/module/bahmnicore/web/v1_0/controller/DoseCalculatorController.java
@@ -1,32 +1,28 @@
 package org.bahmni.module.bahmnicore.web.v1_0.controller;
 
-import org.codehaus.jackson.map.ObjectMapper;
-import org.openmrs.api.APIException;
 import org.openmrs.module.rulesengine.domain.DosageRequest;
 import org.openmrs.module.rulesengine.domain.Dose;
 import org.openmrs.module.rulesengine.engine.RulesEngine;
-import org.openmrs.module.rulesengine.rule.BSABasedDoseRule;
-import org.openmrs.module.rulesengine.rule.WeightBasedDoseRule;
 import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.v1_0.controller.BaseRestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping(value = "/rest/" + RestConstants.VERSION_1 + "/bahmnicore/calculateDose")
 public class DoseCalculatorController extends BaseRestController {
 
     @Autowired
-    private RulesEngine rulesEgine;
+    private RulesEngine rulesEngine;
 
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
     public Dose calculateDose(@RequestParam(value = "dosageRequest") String dosageRequest) throws Exception {
-      //  return new Dose("test",50, Dose.DoseUnit.mg);
-
-        ObjectMapper objectMapper=new ObjectMapper();
-        DosageRequest request= objectMapper.readValue(dosageRequest,DosageRequest.class);
-        return rulesEgine.calculateDose(request);
+        DosageRequest request= new DosageRequest(dosageRequest);
+        return rulesEngine.calculateDose(request);
     }
 }
